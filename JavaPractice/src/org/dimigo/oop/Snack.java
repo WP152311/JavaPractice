86fc07a5bdf0ee618c35fdf8151780f86ec6db4e
박명규(로컬계정) <()@DESKTOP-NIR1TD6>
2017-04-05 12:58:52 +0900

    실습과제 6 1차 제출

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * 	 |_ Snack
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 5.
+ * <pre>
+ *
+ * @author : 박명규(로컬계정)
+ * @version : 1.0
+ */
+public class Snack {
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){}
+	
+	public Snack(String name, String company, int price, int number){
+		this.name = name;
+		this.company = company;
+		this.price = price;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCompany() {
+		return company;
+	}
+
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public int calcPrice(){
+		return price*number;
+	}
+	
+//	public String toString(){
+//		return String.format("이름 : %s\n제조사 : %s\n가격 : %,d원\n개수 : %d개",name,company,price,number);
+//	}
+}
