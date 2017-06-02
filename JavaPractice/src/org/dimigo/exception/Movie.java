d903841f6a87701c1de16c00324c1d083542b77c
박명규(로컬계정) <()@DESKTOP-NIR1TD6>
2017-06-02 15:15:21 +0900

    실습과제 14 1차 제출

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ * 	 |_ Movie
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 6. 2.
+ * <pre>
+ *
+ * @author : 박명규(로컬계정)
+ * @version : 1.0
+ */
+public class Movie {
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title, int limitAge){
+		this.title = title;
+		this.limitAge = limitAge;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public int getLimitAge() {
+		return limitAge;
+	}
+	
+	public void buyTicket(int age){
+		try {
+			if (age < limitAge) {
+				throw new Exception(getTitle() + "은/는 " + getLimitAge() + "세 이상 관람가입니다.");
+			} else {
+				System.out.println(getTitle() + " 즐감하세요.");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+}
